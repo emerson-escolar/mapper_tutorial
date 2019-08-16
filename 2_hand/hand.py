6e1255cebd6e7a208c46b050243ae1cba296a03b
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-08-16 16:27:18 +0900

    simplify script

--- 2_hand/hand.py
+++ 2_hand/hand.py
@@ -16,17 +16,18 @@ lens = data[:,1:2]
 
 plot = True
 if plot:
-    viz.scatter3d(data, lens, colorsMap='viridis', show=False);
+    viz.scatter3d(data, lens, colorsMap='viridis', show=False)
     viz.plt.gca().view_init(elev=90,azim=0)
     viz.axisEqual3D(viz.plt.gca())
     viz.plt.show()
 
-for n in range(5,15):
-    for p in range(2,6):
-        graph = mapper.map(lens, data,
-                           clusterer=lk.LinkageGap(verbose=0),
-                           #clusterer=sklearn.cluster.DBSCAN(eps=0.5,min_samples=1),
-                           cover=km.Cover(n_cubes=n, perc_overlap=p*0.1));
-        name = "n{}_p{}".format(n,p)
-        mapper.visualize(graph, color_function=lens,
-                         path_html="hand_only_" + name + ".html",title="hand, " +name);
+
+n = 7
+p = 0.2
+graph = mapper.map(lens, data,
+                   clusterer=lk.LinkageGap(verbose=0),
+                   cover=km.Cover(n_cubes = n, perc_overlap = p))
+
+name = "n{}_p{}".format(n,p)
+mapper.visualize(graph, color_function=lens,
+                 path_html="hand_only_" + name + ".html",title="hand, " +name)
