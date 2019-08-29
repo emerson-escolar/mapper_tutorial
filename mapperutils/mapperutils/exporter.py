ccea7aafdfb0e2bc2e3ebd648520a588f63d18c9
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-08-29 15:26:47 +0900

    add exporter

---
+++ mapperutils/mapperutils/exporter.py
@@ -0,0 +1,209 @@
+import numpy
+import pandas
+import copy
+import networkx as nx
+
+import json
+import kmapper as km
+
+
+### ******************** networkx-based outputs ********************
+
+def nxmapper_append_node_member_data(nxgraph, extra_data, transforms=None):
+    """
+    Assumptions
+    -----------
+    Each node in nxgraph has 'membership' data that contains indices of original observations.
+
+    Parameters
+    ----------
+    extra_data : dict of dicts {key : {index : data}}
+        For each node, for each *key*, append list of *data* corresponding to its list of member *index*es
+
+    transforms: dict {key : function}
+        For each *key*, apply *function* to the extra_data appended to nodes
+    """
+    for key, data_map in extra_data.items():
+        if transforms == None or key not in transforms:
+            fun = (lambda x:x)
+        else:
+            fun = transforms[key]
+        for node, membership in nxgraph.nodes.data('membership'):
+            nxgraph.nodes[node][key] = fun([data_map[k] for k in membership])
+
+    return nxgraph
+
+
+def nxmapper_append_flare_numbers(nxgraph):
+
+    choices = ((nx.centrality.harmonic_centrality, "H"),
+               (nx.centrality.closeness_centrality, "C"),
+               (nx.centrality.betweenness_centrality, "B")
+               )
+
+    for fun, code in choices:
+        centrality = fun(nxgraph)
+        flares = flare_tree.flare_detect(nxgraph, centrality, prune_threshold=0.01)
+
+        label = code + 'flare'
+        for idx, flare in enumerate(flares):
+            for node in flare.nodes:
+                nxgraph.nodes[node][label] = idx
+
+        label = code + 'centrality'
+        for node, cen in centrality.items():
+            nxgraph.nodes[node][label] = cen
+
+    return nxgraph
+
+
+
+def nxmapper_append_edge_member_data(nxgraph, extra_data, transforms=None):
+    """
+    Assumptions
+    -----------
+    Each edge in nxgraph has 'membership' data which is a list of indices of original observations.
+
+    Parameters
+    ----------
+    extra_data : dict of dicts {key : {index : data}}
+        For each edge, for each *key*, append list of *data* corresponding to its list of member *index*es
+
+    transforms: dict {key : function}
+        For each *key*, apply *function* to the extra_data appended to edges
+    """
+    for key, data_map in extra_data.items():
+        if transforms == None or key not in transforms:
+            fun = (lambda x:x)
+        else:
+            fun = transforms[key]
+        for u, v, membership in nxgraph.edges.data('membership'):
+            nxgraph.edges[(u,v)][key] = fun([data_map[k] for k in membership])
+
+    return nxgraph
+
+
+def nxmapper_append_basic_data(nxgraph, counts=True, weights=True, flares=False):
+    """
+    Convenience function for appending networkx format mapper graph with counts and weights
+
+    Assumption
+    ----------
+    Each node in nxgraph has 'membership' data that contains indices of original observations.
+
+    Parameters
+    ----------
+    counts : bool
+        whether or not to append membership 'count' data to nodes and edges, and 'weight' data to edges.
+
+    weights : bool
+        whether or not to append membership 'weight' data to edges
+
+    flares : bool
+        whether or not to append 'flare index' data to nodes, as computed by persistence-based algorithm
+    """
+
+
+    # Append edge membership
+    for u,v in nxgraph.edges:
+        u_mem = set(nxgraph.nodes[u]['membership'])
+        v_mem = set(nxgraph.nodes[v]['membership'])
+        nxgraph.edges[(u,v)]['membership'] =  list(u_mem.intersection(v_mem))
+
+        if counts:
+            nxgraph.edges[(u,v)]['count'] =  len(nxgraph.edges[(u,v)]['membership'])
+        if weights:
+            nxgraph.edges[(u,v)]['weight'] =  nxgraph.edges[(u,v)]['count'] / len(list(u_mem.union(v_mem)))
+
+    # Append node membership counts
+    if counts:
+        for node, membership in nxgraph.nodes.data('membership'):
+            nxgraph.nodes[node]["count"] = len(membership)
+
+    if flares:
+        nxGraph = nxmapper_append_flare_numbers(nxgraph)
+
+    return nxgraph
+
+
+
+def kmapper_to_nxmapper(graph,
+                        node_extra_data=None, edge_extra_data=None,
+                        node_transforms=None, edge_transforms=None,
+                        counts = True, weights = True, flares=False):
+    """
+    Convenience function for converting kmapper graph to networkx format
+    and appending extra data.
+
+    Assumptions
+    -----------
+    graph is a kmapper output graph
+
+    Parameters
+    ----------
+    node_extra_data : dict of dicts {key : {index : data}}
+        For each node, for each *key*, append list of *data* corresponding to its list of member *index*es
+
+    node_transforms: dict {key : function}
+        For each *key*, apply *function* to the extra_data appended to nodes
+
+    edge_extra_data : dict of dicts {key : {index : data}}
+        For each edge, for each *key*, append list of *data* corresponding to its list of member *index*es
+
+    edge_transforms: dict {key : function}
+        For each *key*, apply *function* to the extra_data appended to edges
+
+    counts : bool
+        whether or not to append membership 'count' data to nodes and edges, and 'weight' data to edges.
+
+    weights : bool
+        whether or not to append membership 'weight' data to edges
+
+    flares : bool
+        whether or not to append 'flare index' data to nodes, as computed by persistence-based algorithm
+    """
+
+    nxGraph = km.adapter.to_nx(graph)
+    nxGraph = nxmapper_append_basic_data(nxGraph, counts, weights, flares)
+    if node_extra_data:
+        nxGraph = nxmapper_append_node_member_data(nxGraph, node_extra_data, node_transforms)
+    if edge_extra_data:
+        nxGraph = nxmapper_append_edge_member_data(nxGraph, edge_extra_data, edge_transforms)
+
+    return nxGraph
+
+
+### ******************** cytoscape json outputs ********************
+
+def cytoscapejson_dump(nxgraph, file):
+    with open(file, 'w') as outfile:
+       json.dump(nx.readwrite.json_graph.cytoscape_data(nxgraph), outfile)
+
+    return nxgraph
+
+
+
+### ******************** text file outputs ********************
+
+def kmapper_text_dump(graph, outfile, labels=None):
+    print("Nodes", file=outfile)
+    for node, members in graph['nodes'].items():
+        print("#" + node, file=outfile)
+        print(len(members), file=outfile)
+
+        if not labels == None:
+            for mem in members:
+                print(labels[mem], file=outfile)
+        else:
+            print(members, file=outfile)
+
+
+    print("Links", file=outfile)
+    for cluster, links in graph['links'].items():
+        print(cluster, file=outfile)
+        print(links, file=outfile)
+
+    if not labels == None:
+        print("Labels", file=outfile)
+        for idx, val in enumerate(labels):
+            print(str(idx) + " " + str(val), file=outfile)
---
+++ mapperutils/mapperutils/tests/test_nxmapper.py
@@ -0,0 +1,77 @@
+import pytest
+
+import mapperutils.exporter as td
+import networkx as nx
+import kmapper as km
+import sklearn.cluster
+
+import numpy as np
+
+@pytest.fixture
+def small_nxgraph():
+    G = nx.Graph()
+    G.add_node(1,membership=[0,1,3])
+    G.add_node(2,membership=[1,2,3])
+    G.add_edge(1,2,membership=[1,3])
+
+    extra_data = {'colors' : ['red', 'green', 'blue', 'black'],
+                  'day' : {0:10,1:20,2:30,3:40}}
+    transforms = {'colors': (lambda x : "".join(x)), 'day' : min}
+
+    return G, extra_data, transforms
+
+
+def test_nxmapper_node_data(small_nxgraph):
+    G,extra_data,_ = small_nxgraph
+    td.nxmapper_append_node_member_data(G, extra_data)
+
+    assert (G.nodes[1]['colors'] == ['red','green','black'])
+    assert (G.nodes[2]['colors'] == ['green','blue','black'])
+
+    assert (G.nodes[1]['day'] == [10,20,40])
+    assert (G.nodes[2]['day'] == [20,30,40])
+
+
+def test_nxmapper_node_data_transformed(small_nxgraph):
+    G,extra_data,transforms = small_nxgraph
+    td.nxmapper_append_node_member_data(G, extra_data, transforms)
+
+    assert (G.nodes[1]['colors'] == 'redgreenblack')
+    assert (G.nodes[2]['colors'] == 'greenblueblack')
+
+    assert (G.nodes[1]['day'] == 10)
+    assert (G.nodes[2]['day'] == 20)
+
+
+def test_nxmapper_edge_data(small_nxgraph):
+    G,extra_data,_ = small_nxgraph
+    td.nxmapper_append_edge_member_data(G, extra_data)
+
+    assert (G.edges[(1,2)]['colors'] == ['green','black'])
+    assert (G.edges[(1,2)]['day'] == [20,40])
+
+
+def test_nxmapper_edge_data_transformed(small_nxgraph):
+    G,extra_data,transforms = small_nxgraph
+    td.nxmapper_append_edge_member_data(G, extra_data, transforms)
+
+    assert (G.edges[(1,2)]['colors'] == 'greenblack')
+    assert (G.edges[(1,2)]['day'] == 20)
+
+
+
+
+def test_kmapper_sample():
+    data = np.array([[0],[1],[2]])
+    lens = data
+
+    graph = km.KeplerMapper().map(data, data, clusterer=sklearn.cluster.DBSCAN(eps=1, min_samples=0), cover=km.Cover(n_cubes=2,perc_overlap=0.5))
+    nxgraph = td.kmapper_to_nxmapper(graph)
+    assert len(nxgraph.edges) == 1
+    assert len(nxgraph.nodes) == 2
+
+    for _,_,data in nxgraph.edges.data():
+        assert 'membership' in data
+
+    for _,data in nxgraph.nodes.data():
+        assert 'membership' in data
