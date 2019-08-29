0d38ec367429ee55ad205c7dfbc6ffce0eb609f6
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2019-08-29 17:50:54 +0900

    add a cat

---
+++ 4_cat/cat.py
@@ -0,0 +1,31 @@
+import numpy as np
+import sklearn
+
+import trimesh
+
+import kmapper as km
+import mapperutils.linkage_gap as lk
+import mapperutils.visualization as viz
+import mapperutils.filters as filt
+
+
+mapper = km.KeplerMapper(verbose=0)
+
+def do_analysis(data, lens, name_prefix, nc, po):
+    name = "{}_n{}_o{}".format(name_prefix, nc, po)
+    graph = mapper.map(lens,
+                       data,
+                       clusterer = lk.LinkageGap(verbose=0),
+                       cover=km.Cover(n_cubes=nc, perc_overlap=po))
+    mapper.visualize(graph,
+                     color_function=lens,
+                     path_html = name + "_cat.html",
+                     title= name + "_cat")
+
+cat = trimesh.load_mesh("../0_data/cat/cat-reference-simplified.obj")
+data = cat.vertices
+
+
+lens = data[:,1:2]
+viz.scatter3d(data, lens,colorsMap='viridis')
+do_analysis(data, lens, "cat_y_", 8, 0.3)
