a08853cb8f1bacf053388f6e988ec7f60ca3c618
Emerson Escolar <38313244+emerson-escolar@users.noreply.github.com>
2020-08-28 17:38:49 +0900

    fix travel script

--- 6_travel/travel_cyto_ans.py
+++ 6_travel/travel_cyto_ans.py
@@ -9,19 +9,28 @@ import mapperutils.linkage_gap as lk
 import mapperutils.visualization as viz
 import mapperutils.filters as filt
 
+import mapperutils.exporter as expo
 
 mapper = km.KeplerMapper(verbose=0)
 
 def do_analysis(data, lens, cf, name_prefix, nc, po):
     name = "{:s}_n{:d}_o{:.2f}".format(name_prefix, nc, po)
     graph = mapper.map(lens,
-                       data,
+                       data.values,
                        clusterer = lk.LinkageGap(verbose=0, metric='euclidean'),
                        cover=km.Cover(n_cubes=nc, perc_overlap=po))
     mapper.visualize(graph,
                      color_function = cf,
                      path_html = name + "_travel.html",
                      title= name+"_travel")
+
+    extra_data = {x: list(data.loc[:,x]) for x in data.columns}
+    extra_transforms = {x : np.mean for x in extra_data}
+
+    nx_graph = expo.kmapper_to_nxmapper(graph, node_extra_data = extra_data,
+                                        node_transforms = extra_transforms)
+    expo.cytoscapejson_dump(nx_graph, name + "_travel.cyjs")
+
 
 raw = pandas.read_csv("../0_data/travel/tripadvisor_review.csv", index_col = 0)
 category_map = {
@@ -36,18 +45,16 @@ category_map = {
     "Category 9": "theaters",
     "Category 10": "religious_institutions"}
 raw = raw.rename(columns = category_map)
-# normalized = (raw - raw.mean()) / raw.std()
-# data = normalized.values
-data = raw
+normalized = (raw - raw.mean()) / raw.std()
+data = normalized
 
 # Filter function:
 lens = skd.PCA(n_components=2).fit_transform(data.values)
 
 # FOR USE IN COLOR in output:
-# cf = np.array(data.mean(axis=1))
-cf = np.array(data.loc[:,"art_galleries"])
+cf = np.array(data.mean(axis=1))
 
 
-p = 5
-n = 5
-do_analysis(data.values, lens, cf, "euc_pca2d" + "_art", n, 0.1*p)
+for n in range(4,11,2):
+    for p in range(35,51,5):
+        do_analysis(data, lens, cf, "euc_pca2d" + "_mean", n, 0.01*p)
